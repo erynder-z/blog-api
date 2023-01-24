d0588521b7d5627e912884a012f477eff7d8fc88
erynder-z <stfn.bgr@gmail.com>
2023-01-24 21:20:23 +0100

    Protect signup route

--- dist/routes/signup.js
+++ dist/routes/signup.js
@@ -5,7 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.signupRoute = void 0;
 const express_1 = require("express");
+const passport_1 = __importDefault(require("passport"));
 const signup_controller_1 = __importDefault(require("../controllers/signup_controller"));
 exports.signupRoute = (0, express_1.Router)();
-exports.signupRoute.post('/api/signup', signup_controller_1.default);
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2lnbnVwLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vcm91dGVzL3NpZ251cC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQSxxQ0FBaUM7QUFDakMseUZBQXNEO0FBRXpDLFFBQUEsV0FBVyxHQUFHLElBQUEsZ0JBQU0sR0FBRSxDQUFDO0FBRXBDLG1CQUFXLENBQUMsSUFBSSxDQUFDLGFBQWEsRUFBRSwyQkFBTSxDQUFDLENBQUMifQ==
+exports.signupRoute.post('/api/signup', (req, res, next) => {
+    if (req.body.signup_secret !== process.env.SIGNUP_SECRET) {
+        return res.status(401).json({ message: 'Invalid signup secret!' });
+    }
+    next();
+}, passport_1.default.authenticate('signup', { session: false }), signup_controller_1.default);
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2lnbnVwLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vcm91dGVzL3NpZ251cC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQSxxQ0FBa0U7QUFDbEUsd0RBQWdDO0FBQ2hDLHlGQUFzRDtBQUV6QyxRQUFBLFdBQVcsR0FBRyxJQUFBLGdCQUFNLEdBQUUsQ0FBQztBQUVwQyxtQkFBVyxDQUFDLElBQUksQ0FDZCxhQUFhLEVBQ2IsQ0FBQyxHQUFZLEVBQUUsR0FBYSxFQUFFLElBQWtCLEVBQUUsRUFBRTtJQUNsRCxJQUFJLEdBQUcsQ0FBQyxJQUFJLENBQUMsYUFBYSxLQUFLLE9BQU8sQ0FBQyxHQUFHLENBQUMsYUFBYSxFQUFFO1FBQ3hELE9BQU8sR0FBRyxDQUFDLE1BQU0sQ0FBQyxHQUFHLENBQUMsQ0FBQyxJQUFJLENBQUMsRUFBRSxPQUFPLEVBQUUsd0JBQXdCLEVBQUUsQ0FBQyxDQUFDO0tBQ3BFO0lBQ0QsSUFBSSxFQUFFLENBQUM7QUFDVCxDQUFDLEVBQ0Qsa0JBQVEsQ0FBQyxZQUFZLENBQUMsUUFBUSxFQUFFLEVBQUUsT0FBTyxFQUFFLEtBQUssRUFBRSxDQUFDLEVBQ25ELDJCQUFNLENBQ1AsQ0FBQyJ9
--- routes/signup.ts
+++ routes/signup.ts
@@ -1,6 +1,17 @@
-import { Router } from 'express';
+import { Request, Response, NextFunction, Router } from 'express';
+import passport from 'passport';
 import signup from '../controllers/signup_controller';
 
 export const signupRoute = Router();
 
-signupRoute.post('/api/signup', signup);
+signupRoute.post(
+  '/api/signup',
+  (req: Request, res: Response, next: NextFunction) => {
+    if (req.body.signup_secret !== process.env.SIGNUP_SECRET) {
+      return res.status(401).json({ message: 'Invalid signup secret!' });
+    }
+    next();
+  },
+  passport.authenticate('signup', { session: false }),
+  signup
+);
