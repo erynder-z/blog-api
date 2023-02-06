e931a36b1b51776b6bd027ca9e168b20c82e0f8b
erynder-z <stfn.bgr@gmail.com>
2023-02-06 20:33:40 +0100

    Remove unused routes

--- routes/index.ts
+++ routes/index.ts
@@ -1,13 +1,11 @@
 import express from 'express';
 import { authRoute } from './auth';
 import { signupRoute } from './signup';
-import { defaultRoute } from './defaultRoute';
 import { blogRoute } from './blog';
 import { tagRoute } from './tag';
 
 export const routes = express.Router();
 
-routes.use(defaultRoute);
 routes.use(authRoute);
 routes.use(signupRoute);
 routes.use(blogRoute);
